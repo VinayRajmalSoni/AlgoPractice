df881932f2635fdaf57ac087d9b3d58635ff86de
VinayRajmalSoni <iceberg.vinay@gmail.com>
2026-04-15 11:48:41 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num; // exor operation will remove the numbers which occur twice
+        }
+        return res;        
+    }
+}
