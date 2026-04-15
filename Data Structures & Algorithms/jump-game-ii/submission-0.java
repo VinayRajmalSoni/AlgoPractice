cfc46aa17469cb7188bc0507b88401dc29e1ffd7
VinayRajmalSoni <iceberg.vinay@gmail.com>
2026-04-15 12:35:49 -0700

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int jump(int[] nums) {
+        if (nums.length <= 1) return 0;
+        
+        int jumps = 0;
+        int currentEnd = 0;
+        int maxReach = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i > maxReach) {
+                return -1;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);            
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = maxReach;
+            }
+        }        
+        return jumps;
+    }
+}
